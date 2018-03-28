e3312e977ff42aaa3db6d02d851110bfcfdf164c
Lexu <a.birsasteanu@gmail.com>
2018-03-28 20:55:47 +0300

    Adding full app for course 1

--- MobileAcademy1/app/src/main/java/com/lexu/mobileacademy1/MainActivity.java
+++ MobileAcademy1/app/src/main/java/com/lexu/mobileacademy1/MainActivity.java
@@ -2,12 +2,42 @@ package com.lexu.mobileacademy1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AppCompatEditText sourceInput = null;
+    private AppCompatButton button = null;
+    private TextView destinationOutput = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.sourceInput = (AppCompatEditText) findViewById(R.id.source_edit_text);
+        this.button = (AppCompatButton) findViewById(R.id.button);
+        this.destinationOutput = (TextView) findViewById(R.id.destination_text_view);
+
+        this.button.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        v.setClickable(false);
+        v.setFocusable(false);
+
+        String name = this.sourceInput.getText().toString();
+        if(!name.isEmpty()) {
+            String placeholderText = MainActivity.this.getResources().getString(R.string.message_placeholder_text);
+
+            this.destinationOutput.setVisibility(View.VISIBLE);
+            this.destinationOutput.setText(String.format(placeholderText, name));
+        }
+        v.setClickable(true);
+        v.setFocusable(true);
     }
 }
